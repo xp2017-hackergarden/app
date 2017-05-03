27a60030e1d38ee4fc01dc9583ad0433d03f6e7e
Goran Milovanovic <goran.milovanovic@codecentric.de>
2017-05-03 11:59:16 +0200

    Storing token to async storage implemented

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,27 @@ import React, {Component} from 'react';
 import {Store, Persistor} from './AppState';
 import AppNavigation from '../AppNavigation';
 import {Provider} from 'react-redux';
+import {AsyncStorage} from 'react-native';
+
 
 export default class App extends Component {
+  componentWillMount(){
+    AsyncStorage.getItem('registration').then((value) => {
+      console.log(value, "VALUE")
+      this.setState({
+        authToken: JSON.parse(value.authToken)
+      });
+    }, (error) => {
+      console.log(error, 'ERROR');
+    });
+
+  }
+  async loadToken(){
+    return await AsyncStorage.getItem('authToken');
+
+  }
   render() {
+    console.log(this.state, 'STATE');
     return (
       <Provider store={Store} persistor={Persistor}>
         <AppNavigation />
